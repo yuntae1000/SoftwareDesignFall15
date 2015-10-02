2793d0b7a66642da8ab60ec731721c1700cd28d7
Sarah Walters <sarah.walters@students.olin.edu>
2015-10-02 14:35:32 -0400

    MP1 feedback

--- Mini_project1/MP1_1_finalw.py
+++ Mini_project1/MP1_1_finalw.py
@@ -2,8 +2,10 @@
 # using patter. to search from Google
 
 from pattern.web import Google
+# You're not using SEARCH in this file -- better not to import it
 from pattern.web import SEARCH
 
+# You're not using indicoio in this file -- better not to import it
 # using indicoio for polytical and sentiment analysis
 import indicoio
 indicoio.config.api_key = '8d05933c4c2ca769d1e064dfbea1fe8a'
@@ -20,20 +22,22 @@ rawurls=[rawurl_nytimes, rawurl_cbsnews, rawurl_wsj, rawurl_foxnews]
 g=Google()
 
 
-#get the New York Times url 
+#get the New York Times url
 for journal, raw_url_title in zip(journal_names, rawurls):
 
 	#in order to get 30 urls with the keyword, used for-loop
-	for i in range(1,4): 
+	for i in range(1,4):
 
 		# search google results correspoding to the following keyword
-		for result in g.search('Donlad Trump opinion site:'+journal+'.com', start=i):
-		   
-		    print result.url
+		for result in g.search('Donald Trump opinion site:'+journal+'.com', start=i):
+
+		    print result.url # Better not to print all of these -- they're not informative to the person using your program
 		    # append the urls to the rawurl_ array
+		    # ^ Not a useful comment - you're just stating what the next line does.
+		    # Better to say _why_ the next line does what it does
 		    raw_url_title.append(result.url)
-		   
-	print raw_url_title
+
+	print raw_url_title # Again, better not to print all of these -- I'm not going to keep writing this, but it applies throughout your code
 	print len(raw_url_title)
 
 	# saves the keyword to the local file in order to reduce query
@@ -43,3 +47,6 @@ for journal, raw_url_title in zip(journal_names, rawurls):
 	print >>f, raw_url_title
 	f.close()
 
+# It would be better to put everything you're doing in this file into a function,
+# then you can write a "master function" at the end which chains all of the steps
+# in your process together
