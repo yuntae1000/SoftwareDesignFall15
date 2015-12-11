23c9dbbab000070c03a2114f26fa9f9e072853e4
YoonTae Jake Jung <yuntae1000@gmail.com>
2015-12-10 23:44:47 -0500

    toolbox3 machine learning commit

--- toolbox/ml/learning_curve.py
+++ toolbox/ml/learning_curve.py
@@ -18,9 +18,22 @@ test_accuracies = numpy.zeros(len(train_percentages))
 # for consistency with the previous example use model = LogisticRegression(C=10**-10) for your learner
 
 # TODO: your code here
+for i in range(len(train_percentages)):
+	size=0.05+0.05*i # increment size of the training set
+	sum_accuracy=0
+	for j in range(num_trials): #number_times per size
+
+		X_train, X_test, y_train, y_test = train_test_split(data.data, data.target, train_size=size)
+		model = LogisticRegression(C=10**-10)
+		model.fit(X_train, y_train) #get model fit
+		
+		print "Test accuracy %f"%model.score(X_test,y_test)
+		sum_accuracy+=model.score(X_test, y_test)
+	sum_accuracy=sum_accuracy/num_trials # get average of accuracy
+	test_accuracies[i]=sum_accuracy
 
 fig = plt.figure()
 plt.plot(train_percentages, test_accuracies)
-plt.xlabel('Percentage of Data Used for Training')
+plt.xlabel('Percentage of Data Used for Training(%)')
 plt.ylabel('Accuracy on Test Set')
 plt.show()
